361dcdc994d12822ae71cde6e053131c8a3c81d8
Daniele Trifirò <daniele.trifiro@ligo.org>
2014-09-08 22:21:31 -0500

    Finally fixed SNR, verified results against ETG challenge dataset

--- finder.py
+++ finder.py
@@ -367,7 +367,7 @@ def find_spikes_algorithm(data, removed_points, f_sampl, threshold, time_resolut
 			#	SNR^2 = 4*\int_0^\infty |g(f)|^2/Pxx(f) df
 			# Since g(f) is symmetric around f  (time series is real)/
 			
-			spike_psd = np.abs(delta_t*np.fft.rfft(spike.waveform))**2)
+			spike_psd = np.abs(delta_t*np.fft.rfft(spike.waveform))**2
 			spike.SNR = np.sqrt(4.0 * f_sampl/float(spike_width) * (spike_psd/psd).sum() )
 			# Save Spike object
 			spikes.append(spike)
