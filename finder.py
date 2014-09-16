ec2c21d0f1ffa134f0f05a6b9910de0c301cb75b
Daniele Trifirò <daniele.trifiro@ligo.org>
2014-09-16 18:06:38 -0500

    Went back to the old definition for SNR (glitch periodogram/detector PSD)

--- finder.py
+++ finder.py
@@ -387,8 +387,8 @@ def find_spikes_algorithm(data, removed_points, f_sampl, threshold, time_resolut
 			
 			# We don't need the factor of 4 in front of the integral because both spike.psd and psd
 			# are one-sided and are correctly normalized
-			#spike.SNR = np.sqrt( 1.0 * (spike.psd/psd).sum()/psd.size )
-			spike.SNR = np.sqrt( 1.0 * (spike.psd).sum()/psd.size )
+			spike.SNR = np.sqrt( 1.0 * (spike.psd/psd).sum()/psd.size )
+			
 			# Save Spike object
 			spikes.append(spike)
 			
